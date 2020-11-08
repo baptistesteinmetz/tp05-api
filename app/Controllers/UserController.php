dddfd489d5efee684091a581335cf5ae4eac301b
baptiste.steinmetz <baptiste.steinmetz67@gmail.com>
2020-11-08 17:10:34 +0100

    p

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -12,33 +12,33 @@ class UserController {
         $login = $data['login'] ?? "";
         $password = $data['password'] ?? "";
 
-        if(!$login == "bapt" || !$password == "toto") {
+        if($login !== "baptiste" || $password !== "baptiste") {
             $response->getBody()->write(json_encode([
                 "success" => false,
             ]));
             return $response->withHeader("Content-Type", "application/json");
         }
+        else {
+            $issuedAt = time();
+            $payload = [
+                "user" => [
+                    "id" => 1,
+                ],
+                "iat" => $issuedAt,
+                "exp" => $issuedAt + 60,
+            ];
+            $token_jwt = JWT::encode($payload, $_ENV['JWT_SECRET'], "HS256");
+    
+            $response->getBody()->write(json_encode([
+                "success" => true,
+            ]));
+            
+            return $response
+            ->withHeader("Authorization", $token_jwt)
+            ->withHeader("Content-Type", "application/json")
+            ->withHeader('Access-Control-Expose-Headers', '*');
+        }
 
-        $issuedAt = time();
-
-        $payload = [
-            "user" => [
-                "id" => 1,
-            ],
-            "iat" => $issuedAt,
-            "exp" => $issuedAt + 60,
-        ];
-
-        $token_jwt = JWT::encode($payload, $_ENV['JWT_SECRET'], "HS256");
-
-        $response->getBody()->write(json_encode([
-            "success" => true,
-        ]));
-        
-        return $response
-        ->withHeader("Authorization", $token_jwt)
-        ->withHeader("Content-Type", "application/json")
-        ->withHeader('Access-Control-Expose-Headers', '*');
     }
 
     public function register(Request $request, Response $response, array $args) {
